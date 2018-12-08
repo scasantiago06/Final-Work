500556ee6aa433846ff02d7568cfc21e5182cb61
Santiago Cardona <32882744+scasantiago06@users.noreply.github.com>
2018-12-07 21:27:01 -0500

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {	
-	void Update ()
+
+    /// <summary>
+    /// The Update function is responsible to keep the camera on top of the character
+    /// </summary>
+    void Update ()
     {
         transform.position = new Vector3(PlayerController.Pos.x, PlayerController.Pos.y + 5, PlayerController.Pos.z);
     }
