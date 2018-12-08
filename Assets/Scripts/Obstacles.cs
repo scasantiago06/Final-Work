f94a5e419ad8e111b937f10dde32ef2961d59b36
Santiago Cardona <32882744+scasantiago06@users.noreply.github.com>
2018-12-07 22:32:45 -0500

    Update Obstacles.cs

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Obstacles : MonoBehaviour, IReactableObject
 {
+    /// <summary>
+    /// This function relocates the character when it dies and adds one to the death counter
+    /// </summary>
     public void React()
     {
         PlayerController.Pos = PlayerController.spawn.position;
