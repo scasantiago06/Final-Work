c35ff56f263c5d1e5ab4c0b9e1d505b15abb84d8
Santiago Cardona <32882744+scasantiago06@users.noreply.github.com>
2018-12-07 21:48:38 -0500

    Update CreditsController.cs

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -5,17 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class CreditsController : MonoBehaviour
 {
+    /// <summary>
+    /// I call the coroutine that is responsible for loading the main menu
+    /// when the time is complete
+    /// </summary>
     private void Start()
     {
         StartCoroutine(CompletedCredits());
-       // AudioManager.Instance.PlayMusic(0);
     }
 
+    /// <summary>
+    /// This function move the gameobject constantly up
+    /// </summary>
     void Update ()
     {
         transform.Translate(Vector3.up * 60 * Time.deltaTime);
     }
 
+    /// <summary>
+    /// This coroutine waits the seconds that the credits screen takes to finish 
+    /// </summary>
     IEnumerator CompletedCredits()
     {
         yield return new WaitForSeconds(45);
