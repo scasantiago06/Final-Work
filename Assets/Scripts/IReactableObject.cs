ae64d57d98d1335e24deaafdc205cf57b387fd5e
Santiago Cardona <32882744+scasantiago06@users.noreply.github.com>
2018-12-07 22:29:12 -0500

    Update IReactableObject.cs

--- Assets/Scripts/IReactableObject.cs
+++ Assets/Scripts/IReactableObject.cs
@@ -1,4 +1,4 @@
-﻿public interface IReactableObject
+﻿public interface IReactableObject //This interface contains a function that will be used by each class that inherits from this
 {
     void React();
 }
